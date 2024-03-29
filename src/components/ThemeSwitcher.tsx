5070a693595127e2d24483c864d313d65f678873
Omar Atta <omaratta212@gmail.com>
2024-03-29 11:27:32 +0200

    prettier config

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,20 +1,13 @@
 // import { useTheme } from 'next-themes';
-import {DarkModeSwitch} from 'react-toggle-dark-mode';
-import {useDarkMode} from "usehooks-ts";
+import { DarkModeSwitch } from "react-toggle-dark-mode";
+import { useDarkMode } from "usehooks-ts";
 
 const ThemeSwitch = () => {
-    const {isDarkMode, toggle,} = useDarkMode({
-        defaultValue: true,
-    })
+  const { isDarkMode, toggle } = useDarkMode({
+    defaultValue: true,
+  });
 
-
-    return (
-        <DarkModeSwitch
-            checked={isDarkMode}
-            onChange={toggle}
-            size={20}
-        />
-    );
+  return <DarkModeSwitch checked={isDarkMode} onChange={toggle} size={20} />;
 };
 
 export default ThemeSwitch;
